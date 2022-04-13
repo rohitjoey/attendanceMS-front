d9feb5d0e78aad049894e6eb1bee4e4695add4d7
rohitjoey <roht.praz@gmail.com>
2022-04-13 10:56:31 +0545

    github pages single page app solution added

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/attendanceMS-front">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
